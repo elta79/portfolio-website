eeca4e1e4f12af904bde80a0183c2d544c1f68d1
elta79 <earnold@outlook.com>
2023-07-14 10:17:58 -0400

    Changes to Experience
    Adding to experience page.

--- src/pages/Experience.jsx
+++ src/pages/Experience.jsx
@@ -1,8 +1,26 @@
 import React from 'react'
+import '../styles/Experience.css'
 
 function Experience() {
   return (
-    <div>Experience</div>
+    <div className='container--experience'>
+      <h1 className="WIP">THIS PAGE IS A WORK IN PROGRESS...PATIENCE APPRECIATED</h1>
+      <h1>Experience</h1>
+      <h2>Emily Arnold</h2>
+      <h3>Front-end Developer</h3>
+      
+      <div className='education'>        
+        <h4>B.Sc Software Development | 2021 | Hodges University, Naples, FL</h4>
+      </div>
+
+      <div className='container--employment'>
+        <h4>Employment</h4>
+        <p className='employment'>Freelance Web Developer | Present</p>
+        <p className='employment'>Financial Director | Family Birth Services, Inc | 2001-Present</p>
+        <p className='employment'>Web Development Internship | Southwest Florida Regional Tech Partnership | 2017-2019</p>
+        
+      </div>
+    </div>
   )
 }
 
