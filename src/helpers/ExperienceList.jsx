7bc6ad458c2f56728bed47edb3c9ae5b229cc872
elta79 <earnold@outlook.com>
2023-07-16 20:26:13 -0400

    Experience content and style
    Added content and styling to Experience page.

---
+++ src/helpers/ExperienceList.jsx
@@ -0,0 +1,29 @@
+export const ExperienceList = [
+    {
+        key: 1,
+        employmentInfo: 'Freelance Web Developer | Present',
+        project:'NaplesPlacenta.com',
+        addedValue: {
+            1: '•	Designed and deployed a simple website for a small business, resulting in a 15% rise in customer engagement within the first month.',
+            2: '•	Increased customer contact opportunities by implementing a user-friendly contact form using EmailJS, resulting in a 30% boost in inquiries received by email.'
+        },
+    },
+    {
+        key: 2,
+        employmentInfo: 'Financial Director/Office Manager | Family Birth Services, Inc | 2001-Present',
+        project:'',
+        addedValue: {
+            1: '•	Planned, developed, tested, deployed, and maintained several versions of the website (WordPress, Squarespace, HTML, CSS, Bootstrap, JavaScript).',
+            2: '•	Created organizational systems to improve efficiency in the office using free software, software we already owned, or creating small task programs, leading to an annual savings of a minimum of $15,000 by eliminating the need for additional hires and reducing software expenses.'
+        },
+    },
+    {
+        key: 3,
+        employmentInfo: 'Web Development Internship | Southwest Florida Regional Tech Partnership | 2017-2019',
+        project:'Jobs4Techies.com',
+        addedValue: {
+            1: '•	Maintained and updated the website with local tech jobs, resulting in a 60% increase in job postings, enabling job seekers access to more opportunities and improving their chances of employment.',
+            2: '•	Attended job fairs to promote the product, leading to at least a 30% increase in product visibility.'
+        },
+    },
+]
--- src/pages/Experience.jsx
+++ src/pages/Experience.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
+import { useParams } from 'react-router-dom'
 import '../styles/Experience.css'
+import { ExperienceList } from '../helpers/ExperienceList.jsx'
 
 function Experience() {
+  const { id } = useParams()
+  const experience = ExperienceList[id]
   return (
     <div className='container--experience'>
-      <h1 className="WIP">THIS PAGE IS A WORK IN PROGRESS...PATIENCE APPRECIATED</h1>
+      
       <h1>Experience</h1>
       <h2>Emily Arnold</h2>
       <h3>Front-end Developer</h3>
@@ -14,12 +18,29 @@ function Experience() {
       </div>
 
       <div className='container--employment'>
-        <h4>Employment</h4>
-        <p className='employment'>Freelance Web Developer | Present</p>
-        <p className='employment'>Financial Director | Family Birth Services, Inc | 2001-Present</p>
-        <p className='employment'>Web Development Internship | Southwest Florida Regional Tech Partnership | 2017-2019</p>
-        
+        <h4 className='title'>Employment</h4>
+        <h4 className='employment'>Freelance Web Developer | Present</h4>
+        <ul>
+          <li>Designed and deployed a simple website for a small business, resulting in a 15% rise in customer engagement within the first month.</li>
+          <li>Increased customer contact opportunities by implementing a user-friendly contact form using EmailJS, resulting in a 30% boost in inquiries received by email.</li>
+        </ul>
+        <h4 className='employment'>Financial Director/Office Manager | Family Birth Services, Inc | 2001-Present</h4>
+        <ul>
+          <li>Planned, developed, tested, deployed, and maintained several versions of the website (WordPress, Squarespace, HTML, CSS, Bootstrap, JavaScript).  </li>
+          <li>Created organizational systems to improve efficiency in the office using free software, software we already owned, or creating small task programs, leading to an annual savings of a minimum of $15,000 by eliminating the need for additional hires and reducing software expenses.</li>
+          <li>Designed a 3-table data collection and processing system for patient data statistics required by Agency for Healthcare Administration and the Florida Department of Health, resulting in a 50% increase in reporting efficiency.  (Microsoft 365-Access, SQL) </li>
+          <li>Collaborated with the owner to create new services for the patients based on patient requirements, adding value to patient care, resulting in a 26% increase in revenue in two years.</li>
+        </ul>
+        <h4 className='employment'>Web Development Internship | Southwest Florida Regional Tech Partnership | 2017-2019</h4>
+        <ul>
+          <li>Maintained and updated the website with local tech jobs, resulting in a 60% increase in job postings, enabling job seekers access to more opportunities and improving their chances of employment.</li>
+          <li>Attended job fairs to promote the product, leading to at least a 30% increase in product visibility.</li>
+        </ul>
       </div>
+
+        
+        
+      
     </div>
   )
 }
